c2d9040b3332ba2f02a6d81ecc5cc9a8fb8f9fe0
nikotinus <serkin2005@mail.ru>
2022-06-17 01:14:37 +0300

    saved 2.3 lab file

--- Users/alex/code/data-engineering/labs/Module02/homework.sql
+++ Users/alex/code/data-engineering/labs/Module02/homework.sql
@@ -29,3 +29,42 @@ count(1)
 , round(sum(o.profit), 2) as lost_profit
 from public.orders o 
 where o.order_id in (select distinct r.orderid from public."returns" r)
+
+select 
+	round(sum(o.sales),2) as TotalSales
+from public.orders o ;
+-- 2297200.86
+
+select 
+	round(sum(o.profit),2) as TotalProfit
+from public.orders o ;
+-- 286397.02
+
+select 
+	round(sum(o.profit) / sum(o.sales) * 100 ,2)
+from public.orders o; 
+-- 12.47
+
+select 
+	count(distinct o.order_id)
+	, sum(o.sales)
+	, sum(o.profit)
+	, round(sum(o.profit)/count(distinct o.order_id), 2)
+from public.orders o; 
+-- 57.18
+
+select 
+	sum(o.sales)
+	, count(distinct o.customer_id)
+	, round(sum(o.sales) / count(distinct o.customer_id),2)
+from public.orders o ;
+-- 2896.85
+
+--Avg. Discount
+select 
+	round(avg(o.discount) * 100, 2)
+from public.orders o ;
+--15.62
+select 
+	sum(o.discount)/count(1)
+from public.orders o;
